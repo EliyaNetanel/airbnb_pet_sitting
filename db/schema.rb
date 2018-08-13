961542b916551caa90e883506e8c70d5b33bbf5b
ELIYA NETANEL <eliya313@gmail.com>
2018-08-13 17:16:14 +0300

    added a description to sitiins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_091029) do
+ActiveRecord::Schema.define(version: 2018_08_13_141311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_091029) do
     t.boolean "accepted_by_owner", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,6 +57,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_091029) do
     t.string "phone_number"
     t.text "description"
     t.string "photo"
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
