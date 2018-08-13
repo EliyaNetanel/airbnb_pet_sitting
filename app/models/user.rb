37b9ee64b25ff98adbdd33b0c885bc8053a87745
yohav rotsztein <yohavrotsztein@hotmail.com>
2018-08-13 17:06:02 +0300

    Add the seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :pets
+  has_many :pets , dependent: :destroy
   #owner:
   has_many :sittings_for_me, class_name: "Sitting", foreign_key: :owner_id
   #sitter:
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Pet.destroy_all
+Sitting.destroy_all
+
+#users
+yohav = User.create!(name:"Yohav",user_name:"Yoyo10",email:"Yohavrotsztein@hotmail.fr",password:"1234567",address:"21 Ben yehouda",phone_number:"0540987123",description:"I love animals, especially roasted with a pich of salt and pepper")
+billel = User.create!(name:"Hillel",user_name:"Billel02",email:"Billel02@hotmail.fr",password:"123456",address:"13 Dizengoff",phone_number:"0581234098",description:"My mom used to say threat animals like animals , they are only animals after all")
+cricri = User.create!(name:"Christophe",user_name:"cricritkt",email:"cricri@hotmail.fr",password:"123456",address:"132 Dizengoff",phone_number:"0581234498",description:"My mom used to say threat animals like well")
+
+#pets
+pets = Pet.create!(name:"Mimi",species:"Siamese",user:yohav)
+pets = Pet.create!(name:"Popo",species:"St Bernard",user:billel)
+
+#sittings
+sittings = Sitting.create!(start_date:"12/05/18",end_date:"15/05/18",rating:4, sitter:cricri, owner:yohav)
