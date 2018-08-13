fbd657045f0c2ed0e4f81e8c7e90c7c9dacce085
ELIYA NETANEL <eliya313@gmail.com>
2018-08-13 18:39:28 +0300

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
-  get 'sittings/index'
-  get 'sittings/show'
-  get 'sittings/new'
-  get 'sittings/create'
-  get 'sittings/edit'
-  get 'sittings/update'
-  get 'pets/index'
-  get 'pets/show'
-  get 'pets/new'
-  get 'pets/create'
-  get 'pets/edit'
-  get 'pets/update'
-  get 'pets/destroy'
   root to: 'pages#home'
   devise_for :users
-  resources :pets
+
+  resources :pets, except: [:index, :show]
   resources :sittings, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
