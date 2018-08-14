b68b2b71d9123aa1dadfd3753f6d539fc23b4b48
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2018-08-14 15:57:59 +0300

    all good

--- app/controllers/sittings_controller.rb
+++ app/controllers/sittings_controller.rb
@@ -1,14 +1,14 @@
 class SittingsController < ApplicationController
   def index
+    @sittings = Sitting.all
   end
 
-  def show
+  def showS
     @sitting = Sitting.find(params[:id])
     @owner = @sitting.owner
     @pets = @owner.pets
     # @owner = @sitting.owner
   end
-
   def new
   end
 
