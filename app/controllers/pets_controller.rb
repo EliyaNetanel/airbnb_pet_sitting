0ce03bba9dd41a734d00d4d9ec54b7dccae791bd
Guy Pyetan <guypyetan@googlemail.com>
2018-08-16 10:15:15 +0300

    New method in pets controller and new.html.erb Not finished due to priority change

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,5 @@
+# Not finished as priorities changed
+
 class PetsController < ApplicationController
   def index
     @user = current_user
@@ -8,9 +10,16 @@ class PetsController < ApplicationController
   end
 
   def new
+    @pet = Pet.new
   end
 
   def create
+    @pet = Pet.create(pet_params)
+    if @pet.save
+      redirect_to @pets, notice: 'pet was added'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +29,14 @@ class PetsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :info)
   end
 end
